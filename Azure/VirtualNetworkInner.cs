c5420fd7c5dfa783038a6cf2d62d8d99febf3c4d
Antonio Santana <antonio.santana.ir@outlook.com>
2019-04-22 11:59:43 -0700

    Create VirtualNetworkInner.cs

---
+++ Azure/VirtualNetworkInner.cs
@@ -0,0 +1,39 @@
+/* 
+  Include these "using" directives...
+  
+  using Microsoft.Azure.Management.Network.Fluent;
+  using Microsoft.Azure.Management.Network.Fluent.Models;
+*/
+using (NetworkManagementClient client = new NetworkManagementClient(credentials))
+{
+    // Define VNet
+    VirtualNetworkInner vnet = new VirtualNetworkInner()
+    {
+        Location = "West US",
+        AddressSpace = new AddressSpace()
+        {
+            AddressPrefixes = new List<string>() { "0.0.0.0/16" }
+        },
+
+        DhcpOptions = new DhcpOptions()
+        {
+            DnsServers = new List<string>() { "1.1.1.1", "1.1.2.4" }
+        },
+
+        Subnets = new List<Subnet>()
+        {
+            new Subnet()
+            {
+                Name = subnet1Name,
+                AddressPrefix = "1.0.1.0/24",
+            },
+            new Subnet()
+            {
+                Name = subnet2Name,
+               AddressPrefix = "1.0.2.0/24",
+            }
+        }
+    };
+    
+    await client.VirtualNetworks.CreateOrUpdateAsync(resourceGroupName, vNetName, vnet);
+}
