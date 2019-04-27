8e50f70c675c0733064f11f8a062f6cacd225809
Antonio Santana <antonio.santana.ir@outlook.com>
2019-04-27 12:44:37 -0700

    Created sln
    and added orig file to directory.

---
+++ Sanfoundry/FahrenheitToCelsius/FahrenheitToCelsius/Program.cs
@@ -0,0 +1,37 @@
+﻿// SOURCE: https://www.sanfoundry.com/csharp-program-binaryreader/
+// AUTHOR: Sanfoundry
+// FILENAME: FahrenheitToCelsius
+// PURPOSE: C# Program to Convert Fahrenheit to Celsius.
+// STUDENT: Antonio Santana
+// DATE: 19 April 2019
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FahrenheitToCelsius
+{
+    class Celsius
+    {
+        static void Main(string[] args)
+        {
+            // Declare celsius as double and ask for user input.
+            double celsius;
+            Console.Write("Enter Fahrenheit temperature : ");
+
+            // Declare fahrenheit as double, store user input into fahrenheit.
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            // Convert fahrenheit to celsius and provide user output.
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            //TODO clean up output
+            Console.WriteLine("The converted Celsius temperature is" + celsius);
+
+            //TODO create loop
+            //TODO create exit statement.
+            Console.ReadLine();
+        }
+    }
+}
