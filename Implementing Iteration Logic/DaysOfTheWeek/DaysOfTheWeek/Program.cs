4b11fc91a3ca116ca7e238e32f66e9c96a6314ec
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2019-05-03 09:13:57 -0700

    Add files via upload

---
+++ Implementing Iteration Logic/DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a "days" local variable.
+            DaysOfTheWeek days = new DaysOfTheWeek();
+
+            // Iterate each day in days and print to string.
+            foreach (string day in days)
+            {
+                Console.Write(day + " ");
+            }
+            // Output: Sun Mon Tue Wed Thu Fri Sat
+            Console.ReadKey();
+        }
+
+        // DayOfTheWeek class
+        public class DaysOfTheWeek : IEnumerable
+        {
+            // Create a string array of days.
+            private string[] days = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            public IEnumerator GetEnumerator()
+            {
+                for (int index = 0; index < days.Length; index++)
+                {
+                    // Yield each day of the week.
+                    yield return days[index];
+                }
+            }
+        }
+    }
+}
