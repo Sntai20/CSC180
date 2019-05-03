85955ff449726d29c58d88ebd1df5648f30a1301
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2019-05-03 09:35:36 -0700

    Add files via upload

---
+++ Implementing Iteration Logic/foreachArray/foreachArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreachArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of numbers.
+            int[] numbers = { 4, 5, 6, 1, 2, 3, -2, -1, 0 };
+
+            // Iterate through number and print to screen.
+            foreach (int i in numbers)
+            {
+                System.Console.Write("{0} ", i);
+            }
+            // Output: 4 5 6 1 2 3 -2 -1 0
+        }
+    }
+}
