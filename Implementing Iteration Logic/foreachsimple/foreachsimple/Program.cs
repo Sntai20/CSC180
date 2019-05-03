6e198015ffdc5b3026f15722db2bb258c3ccd49e
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2019-05-03 09:00:51 -0700

    Add files via upload

---
+++ Implementing Iteration Logic/foreachsimple/foreachsimple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreachsimple
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Iterate through the EvenSequence then convert to string and print to screen.
+            foreach (int number in EvenSequence(0, 18))
+            {
+                Console.Write(number.ToString() + " ");
+            }
+            // Output: 6 8 10 12 14 16 18
+            Console.ReadKey();
+        }
+
+        public static System.Collections.Generic.IEnumerable<int>
+    EvenSequence(int firstNumber, int lastNumber)
+        {
+            // Yield even numbers in the range.
+            for (int number = firstNumber; number <= lastNumber; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    yield return number;
+                }
+            }
+        }
+    }
+}
+
+
