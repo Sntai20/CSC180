73228e0eb0ad273441e33639f5be43f932b57a76
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-04 16:13:30 -0700

    new folders

---
+++ 1 Data Types, Operators, and Expressions/CalculateAcceleration/CalculateAcceleration/Program.cs
@@ -0,0 +1,28 @@
+﻿// SOURCE: https://www.sanfoundry.com
+// AUTHOR: Sanfoundry
+// FILENAME: CalculateAcceleration
+// PURPOSE: C# Program to Calculate Acceleration.
+// STUDENT: Antonio Santana
+// DATE: 19 April 2019
+
+using System;
+
+namespace CalculateAcceleration
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int v, t, acc;
+            Console.WriteLine("Enter the Velocity : ");
+            v = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Time : ");
+            t = int.Parse(Console.ReadLine());
+            acc = v / t;
+            Console.WriteLine("Acceleration : {0}", acc);
+        }
+    }
+}
+
+
+
