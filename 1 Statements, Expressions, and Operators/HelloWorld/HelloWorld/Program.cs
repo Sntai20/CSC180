374c794df9bdf8194d16d8ec682ae65781a565ca
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-04 15:22:51 -0700

    first

---
+++ 1 Statements, Expressions, and Operators/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// SOURCE: https://www.microsoft.com
+// AUTHOR: Visual Studio
+// FILENAME: HelloWorld
+// PURPOSE: My first simple C# Program.
+// STUDENT: Antonio Santana
+// DATE: 17 April 2019
+// Changes: 
+using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
