7f0f2cc48ea1e5301aea1c9ff3842cbd96079bc6
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-04 09:37:47 -0700

    Order of Operations

---
+++ 1 Statements, Expressions, and Operators/OrderOfOperations/OrderOfOperations/Program.cs
@@ -0,0 +1,57 @@
+﻿// SOURCE: https://www.microsoft.com
+// AUTHOR: Microsoft
+// FILENAME: OrderOfOperations
+// PURPOSE: A simple C# Program to demonstrate Order Of Operations.
+// STUDENT: Antonio Santana
+// DATE: 1 May 2019
+// Changes: 
+using System;
+
+namespace OrderOfOperations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+
+            OrderPrecedence();
+
+        }
+
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+            Console.WriteLine(h);
+        }
+    }
+}
