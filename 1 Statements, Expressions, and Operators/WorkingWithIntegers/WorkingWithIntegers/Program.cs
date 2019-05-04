071e0354a4d04a670a3c7e7da03e3372ca211611
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-04 09:30:50 -0700

    Created WorkingWithIntegers

---
+++ 1 Statements, Expressions, and Operators/WorkingWithIntegers/WorkingWithIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿// SOURCE: https://www.microsoft.com
+// AUTHOR: Microsoft
+// FILENAME: WorkingWithIntegers
+// PURPOSE: A simple C# Program to demonstrate integers.
+// STUDENT: Antonio Santana
+// DATE: 1 May 2019
+// Changes: 
+using System;
+
+namespace WorkingWithIntegers
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+        }
+    }
+}
---
+++ 3 Implementing Classes and Structs/ProgramGuide/ProgramGuide/Program.cs
@@ -0,0 +1,54 @@
+﻿// SOURCE: https://www.microsoft.com
+// AUTHOR: Microsoft
+// FILENAME: ProgramGuide
+// PURPOSE: A simple C# Program to demonstrate a class and construct.
+// STUDENT: Antonio Santana
+// DATE: 1 May 2019
+// Changes: 
+using System;
+
+namespace ProgramGuide
+{
+    // Another class definition that contains Main, the program entry point.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type CustomClass.
+            CustomClass custClass = new CustomClass();
+
+            // Set the value of the public property.
+            custClass.Number = 27;
+
+            // Call the public method.
+            int result = custClass.Multiply(4);
+            Console.WriteLine($"The result is {result}.");
+        }
+
+        // Class definition.
+        public class CustomClass
+        {
+            // Class members.
+            //
+            // Property.
+            public int Number { get; set; }
+
+            // Method.
+            public int Multiply(int num)
+            {
+                return num * Number;
+            }
+
+            // Instance Constructor.
+            public CustomClass()
+            {
+                Number = 0;
+            }
+        }
+        // The example displays the following output:
+        //      The result is 108.
+    }
+}
+
+
+
