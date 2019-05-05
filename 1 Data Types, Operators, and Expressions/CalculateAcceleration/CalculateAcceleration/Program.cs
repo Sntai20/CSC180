7149d95db29b6b98b508ec358ffde328fbfb59f1
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-05 08:50:40 -0700

    Update Program.cs
    Added comments and changed data types from ints to double.

--- 1 Data Types, Operators, and Expressions/CalculateAcceleration/CalculateAcceleration/Program.cs
+++ 1 Data Types, Operators, and Expressions/CalculateAcceleration/CalculateAcceleration/Program.cs
@@ -4,6 +4,7 @@
 // PURPOSE: C# Program to Calculate Acceleration.
 // STUDENT: Antonio Santana
 // DATE: 19 April 2019
+// Changes: Data types to a double and added expected output.
 
 using System;
 
@@ -13,16 +14,30 @@ namespace CalculateAcceleration
     {
         public static void Main(string[] args)
         {
-            int v, t, acc;
+            // Declares int data types.
+            double v, t, acc;
+
+            // Request and stores user input to v as in int.
             Console.WriteLine("Enter the Velocity : ");
-            v = int.Parse(Console.ReadLine());
+            v = double.Parse(Console.ReadLine());
+
+            // Request and stores user input to t as an int.
             Console.WriteLine("Enter the Time : ");
-            t = int.Parse(Console.ReadLine());
+            t = double.Parse(Console.ReadLine());
+
+            // Uses a divide operator to calculate acceleration.
             acc = v / t;
             Console.WriteLine("Acceleration : {0}", acc);
         }
     }
 }
-
+/*
+         Output:
+            Enter the Velocity : 
+            100
+            Enter the Time : 
+            60
+            Acceleration : 1.66666666666667         
+      */
 
 
