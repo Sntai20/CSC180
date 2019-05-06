ef5c990870c6ebcc15caf33f3d48d9065b468b23
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-05 17:11:46 -0700

    Updated

---
+++ 1 Data Types, Operators, and Expressions/CalculatePowerExponent/CalculatePowerExponent/Program.cs
@@ -0,0 +1,28 @@
+﻿// SOURCE: https://www.sanfoundry.com
+// AUTHOR: Sanfoundry
+// FILENAME: CalculatePowerExponent
+// PURPOSE: C# Program to Calculate the power exponent value.
+// STUDENT: Antonio Santana
+// DATE: 1 April 2019
+// Changes: 
+
+using System;
+
+namespace CalculatePowerExponent
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double m, n;
+            Console.WriteLine("Enter the Number : ");
+            m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Exponent : ");
+            n = double.Parse(Console.ReadLine());
+            double value1 = Math.Pow(m, n);
+            Console.WriteLine("Result : {0}", value1);
+            Console.ReadLine();
+        }
+    }
+}
+
