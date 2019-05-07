5ceb77f95d5cc6e9f8b9d51a771ce3ac40cff1a4
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-06 17:55:10 -0700

    Updated

--- 3 Implementing Iteration Logic/forloop.cs
+++
@@ -1,6 +0,0 @@
-﻿// Executes a statement or a block of statements while a specified Boolean expression evaluates to true.
-
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine(i);
-}
---
+++ 5 Exception Handling/ExceptionTest/ExceptionTest/Program.cs
@@ -0,0 +1,39 @@
+﻿// SOURCE: https://www.microsoft.com
+// AUTHOR: Microsoft
+// FILENAME: ExceptionTest
+// PURPOSE: C# Program to demonstrate exception handling.
+// STUDENT: Antonio Santana
+// DATE: 1 May 2019
+// Changes: 
+
+using System;
+
+namespace ExceptionTest
+{
+    class MainClass
+    {
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+                throw new System.DivideByZeroException();
+            return x / y;
+        }
+        static void Main()
+        {
+            // Input for test purposes. Change the values to see
+            // exception handling behavior.
+            double a = 98, b = 0;
+            double result = 0;
+
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine("{0} divided by {1} = {2}", a, b, result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Attempted divide by zero.");
+            }
+        }
+    }
+}
