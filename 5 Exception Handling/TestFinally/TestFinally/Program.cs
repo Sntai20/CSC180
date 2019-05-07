336106a30f32077c958e426e52856da3357f4e7c
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-06 18:02:38 -0700

    Update

---
+++ 5 Exception Handling/TestFinally/TestFinally/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace TestFinally
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            try
+            {
+                using (var sw = new StreamWriter(@"C:\test\test.txt"))
+                {
+                    sw.WriteLine("Hello");
+                }
+            }
+            // Put the more specific exceptions first.
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            // Put the least specific exception last.
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Done");
+        }
+
+        static void TestFinally()
+        {
+            System.IO.FileStream file = null;
+            //Change the path to something that works on your machine.
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(@"C:\file.txt");
+
+            try
+            {
+                file = fileInfo.OpenWrite();
+                file.WriteByte(0xF);
+            }
+            finally
+            {
+                // Closing the file allows you to reopen it immediately - otherwise IOException is thrown.
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+
+            try
+            {
+                file = fileInfo.OpenWrite();
+                System.Console.WriteLine("OpenWrite() succeeded");
+            }
+            catch (System.IO.IOException)
+            {
+                System.Console.WriteLine("OpenWrite() failed");
+            }
+        }
+    }
+}
+
