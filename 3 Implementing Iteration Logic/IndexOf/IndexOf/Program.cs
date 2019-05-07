3f92fee884bd09e793939277463fc847f024cf48
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-07 06:37:29 -0700

    Update

---
+++ 3 Implementing Iteration Logic/IndexOf/IndexOf/Program.cs
@@ -0,0 +1,56 @@
+﻿// SOURCE: https://www.dotnetperls.com/array
+// AUTHOR: Sam Allen
+// FILENAME: IndexOf
+// PURPOSE: C# Program to demonstrate the IndexOf method.
+// STUDENT: Antonio Santana
+// DATE: 19 April 2019
+// Changes: 
+
+using System;
+
+namespace IndexOf
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            // The program now strings inputs from the user.
+            // Instead of rewriting Console.ReadLine() three
+            // times, the for loop can be used to store and
+            // read the strings three times in one line.
+            string[] animals = new string[3];
+            Console.WriteLine("Enter 3 animals:");
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i] = Console.ReadLine();
+            }
+
+            // With the animals index stored with user's input,
+            // the program requests which index the user would
+            // like to see (the numeric position). By typing
+            // 'Cat', the program will return 1 since the Cat
+            // is in index 1 of the array.
+            Console.WriteLine("Which index would you like to access?");
+            string input = Console.ReadLine();
+            int inputIndex = Array.IndexOf(animals, input);
+            Console.WriteLine(inputIndex);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+/*
+        Modified Output:
+        Enter 3 animals:
+        Dog
+        Cat
+        Bat
+        Which index would you like to see?
+        Cat
+        1
+        As with the original program, if the
+        program attempts to access an index value
+        that's not within the array (0-2), the
+        program will output -1.
+*/
---
+++ 3 Implementing Iteration Logic/ReversePalindrome/ReversePalindrome/Program.cs
@@ -0,0 +1,72 @@
+﻿// SOURCE: https://www.sanfoundry.com
+// AUTHOR: Manish Bhojasia
+// FILENAME: GetFileTime
+// PURPOSE: C# Program identifies if user's number is a palindrome.
+// STUDENT: Antonio Santana
+// DATE: 6 May 2019
+// Changes: 
+
+
+using System;
+
+namespace ReversePalindrome
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+
+            int num, temp, remainder, reverse = 0;
+            Console.WriteLine("Enter an integer \n");
+            num = int.Parse(Console.ReadLine());
+
+            // The temp variable will initially share the same
+            // value as num. 
+            temp = num;
+
+            // The while loop will execute as long as the 'num'
+            // variable is greater than 0.
+            while (num > 0)
+            {
+                // The remainder variable will return the remainder
+                // of an input value divided by 10. Then the
+                // program will determine the value of reverse
+                // variable by multiplying it to 10 and adding
+                // the value of remainder. The original reverse
+                // value used inside the reverse variable will
+                // be 0. The num value is then assigned to the
+                // value returned when it's divided by 10, i.e.,
+                // 'num /= 10' means 'num = num/10'.
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+
+            // The Console will print the temp value which was
+            // the value of the original input while the reverse
+            // value will print the value returned from calculations
+            // executed during the while loop.
+            Console.WriteLine("Given number is = {0}", temp);
+            Console.WriteLine("Its reverse is  = {0}", reverse);
+
+            // The condition statement checks to see if the temp
+            // and reverse variables are equal and returns true.
+            // If they are, the program prints out that it's a
+            // palindrome.
+            if (temp == reverse)
+                Console.WriteLine("Number is a palindrome \n");
+            else
+                Console.WriteLine("Number is not a palindrome \n");
+            Console.ReadLine();
+        }
+    }
+}
+
+/*      
+        Example output:
+        Enter an integer
+        252
+        Given number is = 252
+        Its reverse is  = 252
+        Number is a palindrome
+*/
