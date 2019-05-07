617acb4f4f2f403a416683aa125b3722e3f07e90
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-06 18:06:19 -0700

    DoWhileLoop

---
+++ 3 Implementing Iteration Logic/DoWhile/DoWhile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+        }
+    }
+}
