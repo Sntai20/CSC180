b9634ad0ca3a4dcddcc5f25d05a505c82284c647
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-07 18:16:43 -0700

    Completed comments

--- 1 Data Types, Operators, and Expressions/CalculatePowerExponent/CalculatePowerExponent/Program.cs
+++ 1 Data Types, Operators, and Expressions/CalculatePowerExponent/CalculatePowerExponent/Program.cs
@@ -14,15 +14,31 @@ namespace CalculatePowerExponent
     {
         public static void Main(string[] args)
         {
+            // Declare two variables as a double data type.
             double m, n;
+
+            // Ask for the user's input and store the value in m.
             Console.WriteLine("Enter the Number : ");
             m = double.Parse(Console.ReadLine());
+
+            // Ask for the user's input and store the value in n.
             Console.WriteLine("Enter the Exponent : ");
             n = double.Parse(Console.ReadLine());
+
+            // Declare value1 as a double and raise to the n power.
             double value1 = Math.Pow(m, n);
+
+            // Return the value to the user.
             Console.WriteLine("Result : {0}", value1);
             Console.ReadLine();
         }
     }
 }
-
+/*
+Output:
+Enter the Number : 
+2
+Enter the Exponent : 
+3
+Result : 8
+    */
