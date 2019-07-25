a8a035a665269e4596eb103300a950191c7514b8
Antonio Santana <antonio.santana.ir@outlook.com>
2019-07-25 06:32:30 -0400

    Update.

---
+++ ReversChar/ReversChar/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ReversChar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = "";
+            getWords(str);
+            //string str = "uoY detnaw ot wonk ohw I ,ma oreZ ?looC ,lleW tel em nialpxe eht weN dlroW .redrO stnemnrevoG dna snoitaroproc deen elpoep ekil uoy dna .em eW era ...iarumaS eht draobyeK ...syobwoC dna lla esoht rehto elpoep ohw evah on aedi s'tahw gniog no era eht ...elttac ooooM";
+
+            reverseWords(str);
+        }
+
+        private static string getWords(object str)
+        {
+            string filePath = @"/Users/antonio/Documents/GitHub/CSC180/ReversChar/ReversChar/bin/Debugreversme.txt";
+            using (FileStream file = new FileStream(filePath))
+            {
+                StreamReader reader = new StreamReader(file);
+            }
+
+            return reader;
+        }
+
+        // Reverse individual words in a string.
+        public static void reverseWords(string str)
+        {
+            Stack<char> st = new Stack<char>();
+
+            // Traverse given string and push 
+            // all characters to stack until 
+            // we see a space. 
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (str[i] != ' ')
+                {
+                    st.Push(str[i]);
+                }
+
+                // When we see a space, we 
+                // print contents of stack. 
+                else
+                {
+                    while (st.Count > 0)
+                    {
+                        Console.Write(st.Pop());
+                    }
+                    Console.Write(" ");
+                }
+            }
+
+            // Since there may not be 
+            // space after last word. 
+            while (st.Count > 0)
+            {
+                Console.Write(st.Pop());
+
+            }
+        }
+    }
+}
+
+
+
+
+
--- SOReverseMe/SOReverseMe/Program.cs
+++ SOReverseMe/SOReverseMe/Program.cs
@@ -6,86 +6,27 @@ namespace SOReverseMe
     {
         public static void Main()
         {
-            const string s1 = "Bill Gates is the richest man on Earth";
-            const string s2 = "uoY detnaw ot wonk ohw I ,ma oreZ ?looC ,lleW tel em nialpxe eht weN dlroW .redrO stnemnrevoG dna snoitaroproc deen elpoep ekil uoy dna .em eW era ...iarumaS eht draobyeK ...syobwoC dna lla esoht rehto elpoep ohw evah on aedi s'tahw gniog no era eht ...elttac ooooM";
-            const char s3 = 'uoY detnaw ot wonk ohw I ,ma oreZ ?looC ,lleW tel em nialpxe eht weN dlroW .redrO stnemnrevoG dna snoitaroproc deen elpoep ekil uoy dna .em eW era ...iarumaS eht draobyeK ...syobwoC dna lla esoht rehto elpoep ohw evah on aedi s'tahw gniog no era eht...elttac ooooM';
+            string inputString = "htrae no nam tsehcir ehT";
+            string outputString = ReverseString(inputString);
 
-            // In the Main() method, the rev1/rev2 string variables will
-            // call the WordTools class and its .ReverseWords() method
-            // and applied to both s1 and s2 strings that were declared
-            // and assigned the beginning of the method. 'rev1' and 'rev2'
-            // are introduced as new variables because the original s1
-            // and s2 were declared as constant strings.
-            string rev1 = ReverseWords(s1);
-            Console.WriteLine(rev1);
+            Console.WriteLine("Original String: " + inputString); 
+            Console.WriteLine(outputString);
 
-            // Print original
-            Console.WriteLine(s2);
-
-            // Print original
-            Console.WriteLine(s3);
-
-            string rev2 = ReverseWords(s2);
-            Console.WriteLine(rev2);
+            Console.ReadLine();
         }
+     
 
-        private static string ReverseWords(string sentence)
+        private static string ReverseString(string inputString)
         {
-            // The ReverseWords class accepts a string data type
-            // for the 'sentence' variable.
+            string reversedString = "";
 
-            // The 'words' variable is declared as a string
-            // data type that will be an array. The sentence.Split()
-            // will store strings that are split by spaces, e.g.,
-            // "the dog" string will store the strings "the" and "dog".
-            string[] words = sentence.Split(' ');
+            for(int count=inputString.Length-1;count >= 0; count--)
+            {
+                reversedString += inputString[count];
+            }
 
-            // The .Reverse() method will now reverse the stored strings
-            // inside the 'words' array.
-            Array.Reverse(words);
-
-            // Finally, the ReverseWords() method will return
-            // the previously reversed strings in the 'words'
-            // variable and join the strings separated by
-            // a space.
-            return string.Join(" ", words);
-        }
-
-        private static string ReverseCharacters(char s3)
-        {
-            // The ReverseWords class accepts a string data type
-            // for the 'sentence' variable.
-
-            // The 'words' variable is declared as a string
-            // data type that will be an array. The sentence.Split()
-            // will store strings that are split by spaces, e.g.,
-            // "the dog" string will store the strings "the" and "dog".
-            char[] characters = s3(' ');
-
-            // The .Reverse() method will now reverse the stored strings
-            // inside the 'words' array.
-            Array.Reverse(characters);
-
-            // Finally, the ReverseWords() method will return
-            // the previously reversed strings in the 'words'
-            // variable and join the strings separated by
-            // a space.
-            return string.Join(" ", characters);
+            return reversedString;
         }
     }
 }
-/*
-   Source: https://www.dotnetperls.com/reverse-words
-   Author: Sam Allen
-   Summary: This program reverses the order of strings without
-            reversing the strings.
-   Date: 4 May 2019
-   Modifications: ----
- */
 
-
-/*      
-        Example output:
-        Earth on man richest the is Gates Bill
-        mathematician brilliant a was Ramanujan Srinivasa
-*/
